9a6a800acf9be56b67111d3993c8908f1ef283c3
Minura Anuradha <112975973+minuraanuradha@users.noreply.github.com>
2024-03-30 13:20:28 +0530

    user profile up

--- Admin/doc/doc_dashboard.php
+++ Admin/doc/doc_dashboard.php
@@ -1,9 +1,3 @@
-<?php
-session_start();
-if (!isset($_SESSION['id'])) {
-    header("location:doc_login.php");
-}
-?>
 <!--<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,8 +28,8 @@ if (!isset($_SESSION['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/Sahana_Admin/Assets/Doc.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap-grid.rtl.min.css">
+    <link rel="stylesheet" href="./css/doc_Style.css">
 
     <script src="/bootstrap/js/bootstrap.min.js"></script>
 
