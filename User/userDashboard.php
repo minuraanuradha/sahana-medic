89deb8cc0e4a361e82f38aababc061300ec5f367
Minura Anuradha <112975973+minuraanuradha@users.noreply.github.com>
2024-03-30 19:31:20 +0530

    profile page update

--- User/userDashboard.php
+++ User/userDashboard.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
+
+    <link rel="stylesheet" href="./css/profile.css">
 
     <title>index</title>
 </head>
@@ -26,9 +27,9 @@
       <div class="container "  >
           <div class="row " >
               <div class="col-12 col-md-12 set_main d-flex ">
-                  <div class="col-12 col-md-6 set_mini justify-content-center align-iteam-center">
-                <div class="card cardpro" style="width: 18rem;">
-                    <img class="card-img-top" src="./Assets/Logo01.png" alt="Card image cap">
+                  <div class="col-12 col-md-3 set_mini justify-content-center align-iteam-center">
+                <div class="card cardpro" style="width: 15rem;">
+                    <img class="card-img-top p-4" src="./Assets/Logo01.png" alt="Card image cap" style="height:100px,weight:100px">
                         <div class="card-body">
                              <h5 class="card-title">Profile Name</h5>
                         </div>
@@ -43,9 +44,35 @@
                     </div>
                 </div>
                   </div>
-                  <div class="col-12 col-md-6 set_mini justify-content-center align-iteam-center">
+                  <div class="col-12 col-md-3  justify-content-center align-iteam-center">
                     <div class="row ">
                           <button class="btn_ap " type="submit"><a href="Signup.php">Add Appoinments</a></button>
+                    </div>
+                  </div>
+                  <div class="col-12 col-md-6  justify-content-center align-iteam-center">
+                    <div class="row ">
+                        <div class="container-fluid apoinment-cover">
+                        <div class="container ">
+    <h5 class="pro_topic">My Appoinments</h5>
+    <div class="container">
+        <div class="col-2 card_a">
+            10
+        </div>
+        <div class="col-2 card_a">
+            10
+        </div>
+        <div class="col-2 card_a">
+            10
+        </div>
+        <div class="col-2 card_a">
+            10
+        </div>
+        <div class="col-2 card_a">
+            10
+        </div>
+    </div>
+</div>
+                        </div>
                     </div>
                   </div>
               </div>
