732212a77ffe70d982dfe4c14e14772d62a028b8
Minura Anuradha <112975973+minuraanuradha@users.noreply.github.com>
2024-03-31 10:22:59 +0530

    home update 02

--- User/index.php
+++ User/index.php
@@ -10,41 +10,128 @@
     <title>index</title>
 </head>
 <body>
+  <!--------------Nav + page  con 01---------------> 
   <div class="container-fluid  home">
 
-  <!--Nav--> 
+    <!--Nav--> 
     <div class="col-12 ">
-        <?php
+      <?php
         include "nav.php";
-        ?>
+      ?>
     </div>  
 
-  <!--Page Contents-->      
+    <!---------------Page Contents 01-------------->      
     <div class="row ">
-        <div class="container "  >
-            <div class="row " >
-                <div class="col-12 col-md-12 set_main d-flex ">
-                    <div class="col-12 col-md-6 set_mini justify-content-center align-iteam-center">
-                    <h1 class="topic1 text-start">Take Care of your</h1>
-                    <h1 class="topic2 text-start"> HELTH </h1>
-                    <p  class="sub_topic text-start">lasjdvnoksjdv nowkv wkvinwievkw evp zsdv awbv </p>
-                    <div class="row ">
-                        <button class="btn_ap " type="submit"><a href="Signup.php">Add Appoinments</a></button>
-                    </div>
-                    </div>
-                </div>
+      <div class="container "  >
+        <div class="row " >
+          <div class="col-12 col-md-12 set_main d-flex ">
+            <div class="col-12 col-md-5 set_mini justify-content-center align-iteam-center">
+              <h1 class="topic1 text-start">Take Care of your</h1>
+              <h1 class="topic2 text-start"> HELTH </h1>
+              <p  class="sub_topic text-start">lasjdvnoksjdv nowkv wkvinwievkw evp zsdv awbv </p>
+              <div class="row ">
+                <button class="btn_ap " type="submit"><a href="Signup.php">Add Appoinments</a></button>
               </div>
+            </div>
+          </div>
         </div>
-    </div>
-    </div>
+      </div>
     </div>
 
+  </div>
+
+  <!---------------Services-------------->
+  <div class="row container-fluid " style="padding-left:3vw">
     
+      <div class="container"  >
+        <div class="row text-center" >
+            <h1 class="sub_topic1">Our Services</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas a saepe itaque quae. Ratione illum itaque ex quaerat dignissimos non quis, corporis possimus iure totam, aperiam voluptas temporibus facilis quibusdam!</p>
+          <div class="col-12 col-md-12 set_main1 d-flex ">
+
+            <div class="col-12 col-md-3  justify-content-center align-iteam-center">
+              <div class="card text-center" id="card01">
+                <img class="card-img-top" src="./Assets/s1.png" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-12 col-md-3  justify-content-center align-iteam-center">
+              <div class="card text-center" id="card02">
+              <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-3  justify-content-center align-iteam-center">
+              <div class="card text-center" id="card03">
+              <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-12 col-md-3  justify-content-center align-iteam-center">
+              <div class="card text-center" id="card04">
+              <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+  </div>
+
+
+  <!---------------Line-------------->
+  <div id="carouselExampleSlidesOnly" class="carousel slide mt-5  d-flex" data-bs-ride="carousel">
+    <div class="carousel-inner ">
+      <div class="carousel-item active ">
+        <img src="./Assets/p03.jpg" class="d-block w-100  " alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./Assets/p03.jpg" class="d-block w-100" alt="..." >
+      </div>
+      <div class="carousel-item">
+        <img src="./Assets/p03.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+  </div>
+
+  <!---------------Join Us-------------->
+  <div class="row container-fluid mt-5  d-flex " style="padding-left:3vw">
+    
+      <div class="container  "  >
+        <div class="row text-center justify-content-center align-iteam-center" >
+            <h1 class="sub_topic1">Join With Us</h1>
+            <p class="col-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas a saepe itaque quae. Ratione illum itaque ex quaerat dignissimos non quis, corporis possimus iure totam, aperiam voluptas temporibus facilis quibusdam!</p>
+            <div class="card col-8" id="card05">
+                <div class="card-header">
+                <img src="./Assets/Logo04white.png" alt="">
+                </div>
+                <div class="card-body">
+                  <a href="#" class="btn btn-primary btn_ap1">Sign Up</a>
+                </div>
+            </div>
+        </div>
+      </div>
+  </div>
+
+
+
   <!--footer--> 
   <div class="col-12 mt-3 bg-success">
-        <?php
-        include "footer.php";
-        ?>
+    <?php
+      include "footer.php";
+    ?>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
