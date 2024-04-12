f7fcb44b19d22cd49ee2873e8af9a0441a93f3b6
Minura Anuradha <112975973+minuraanuradha@users.noreply.github.com>
2024-04-12 20:22:49 +0530

    A02.0_update : change admin login interface

--- Admin/doc/doc_empadd.php
+++ Admin/doc/doc_empadd.php
@@ -234,6 +234,11 @@
     <script src="./js/demo/chart-area-demo.js"></script>
     <script src="./js/demo/chart-pie-demo.js"></script>
 
+    <!-- Bootstrap JS and jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
