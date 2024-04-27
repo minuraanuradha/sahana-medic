3f4f7f237081688b87ca142af06e9d5b1b602537
Minura Anuradha <112975973+minuraanuradha@users.noreply.github.com>
2024-04-27 20:42:01 +0530

    Update : Update port number for code

--- Model/connection.php
+++ Model/connection.php
@@ -6,9 +6,10 @@ $servername = "localhost";
 $username = "root";
 $password = "";  
 $dbname = "sahana_medic";
+$port = 3377;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
